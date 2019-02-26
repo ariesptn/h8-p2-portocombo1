26938ce901e57071cacc55b9e76785b64c76a2f6
Arie Septian <ariesptn@gmail.com>
2019-02-26 12:21:20 +0700

    cron, bg task

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,14 +1,25 @@
 const jwt = require('jsonwebtoken')
 const models = require('../models')
+const cron = require('node-cron')
+
+let userTokens = {}
 
 async function authentication(req, res, next) {
     try {
-        let decoded = jwt.verify(req.headers.token, process.env.JWT_SECRET)
+        let userCachedData = userTokens[req.headers.token]
+        if (userCachedData) {
+            req.auth = userCachedData
+            return next()
+        }
+        let decoded = jwt.verify(req.headers.token, process.env.JWT_SECRET || 'notsosecret')
         if (decoded) {
-            let userData = await models.User.findOne({ email: decoded.email })
+            let userData = await models.User.findOne({ email: decoded.email }).lean()
             if (userData) {
-                req.auth = decoded
-                next()
+                req.auth = userData
+                userTokens[req.headers.token] = req.auth
+                userTokens[req.headers.token].accessedAt = new Date().getTime()
+                tokenTimeout(req.headers.token)
+                return next()
             } else {
                 throw { message: 'unauthorized user' }
             }
@@ -20,5 +31,32 @@ async function authentication(req, res, next) {
         console.log(err)
     }
 }
+
+function cleanCache() {
+    for (const token in userTokens) {
+        if (new Date().getTime() - userTokens[token].accessedAt > 60 * 60 * 1000) {
+            delete userTokens[token]
+        }
+    }
+}
+
+cron.schedule('19 * * * *', function () {
+    cleanCache()
+})
+
+const kue = require('kue')
+
+function tokenTimeout(token) {
+    const queue = kue.createQueue()
+    queue.create('token-timeout', { token }).save()
+    queue.process('token-timeout', (job, done) => {
+        setTimeout(() => {
+            delete userTokens[job.data.token]
+            done()
+        }, 60 * 60 * 1000)
+    })
+}
+
+kue.app.listen(3001)
 
 module.exports = { authentication, }
